20f6ff9b8e9cd5a8697ba29e0af4c446ecd45a94
Hamilton Pinheiro <hamilton.pinheiro@aubiconmg.local>
2020-06-06 07:46:38 -0300

    https

--- src/pages/videos.jsx
+++ src/pages/videos.jsx
@@ -14,7 +14,7 @@ export default class Videos extends Component {
 
         let { id } = this.props.match.params
 
-        const url = `https://hamiltondeveloper-com-br.umbler.net/videos`
+        const url = `https://pinheirosites-com-br.umbler.net/videos`
 
         fetch(url)
             .then(res => res.json())
