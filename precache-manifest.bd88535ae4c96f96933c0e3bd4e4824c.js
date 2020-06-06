f149d460da8385de48fa9704ef5ae697e0722b70
Hamilton Pinheiro <hamilton.pinheiro@aubiconmg.local>
2020-06-06 08:02:57 -0300

    Updates

---
+++ precache-manifest.bd88535ae4c96f96933c0e3bd4e4824c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ac83615935d7fab245d1ea3568f2b95",
+    "url": "/helpdesk/index.html"
+  },
+  {
+    "revision": "b156ed90a1b0b204b7fe",
+    "url": "/helpdesk/static/css/main.2d38894e.chunk.css"
+  },
+  {
+    "revision": "7755ea9a77c15de632ff",
+    "url": "/helpdesk/static/js/2.569ea5bd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/helpdesk/static/js/2.569ea5bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b156ed90a1b0b204b7fe",
+    "url": "/helpdesk/static/js/main.3cef4f78.chunk.js"
+  },
+  {
+    "revision": "95f7422938f4cfeccdcd",
+    "url": "/helpdesk/static/js/runtime-main.9f05515f.js"
+  },
+  {
+    "revision": "a623bb223e2a55eab8f5e2d60adc984e",
+    "url": "/helpdesk/static/media/aubicon.a623bb22.jpg"
+  }
+]);
